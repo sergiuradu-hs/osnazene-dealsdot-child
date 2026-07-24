6e55d17d63c049d5be202f029fb9b843b850c970
Sergiu <sradu@hubsync.com>
2026-07-24 13:05:21 +0200

    Drzava Tax Grid for mobile

--- wpb_shortcodes/drzava-tax-grid/templates/template.php
+++ wpb_shortcodes/drzava-tax-grid/templates/template.php
@@ -14,6 +14,9 @@
         <?php endif; ?>
         <span class="osn-tax-grid-item__text"><?php echo esc_html( $item['name'] ); ?></span>
       </span>
+      <span class="osn-tax-grid-item__chevron" aria-hidden="true">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/></svg>
+      </span>
     </a>
   <?php endforeach; ?>
 </div>
